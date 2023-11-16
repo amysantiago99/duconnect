da48c667da23ee14a57abba3e702c7dcf344f919
Aastha Chaurasia <114049116+amysantiago99@users.noreply.github.com>
2023-11-16 15:37:07 +0530

    Add files via upload

---
+++ public/js/config.js
@@ -0,0 +1,6 @@
+const config = {
+    CometChatAppId: "248415f4cd58c4fb",
+    CometChatRegion: "IN",
+    CometChatAuthKey: "e8e5d7b5b691b30d7cfbd12f215b7b2d2b4f713d",
+    CometChatAPIKey: "95e5f16a966219d29af02d77ba5fe82995966b92"
+  };
